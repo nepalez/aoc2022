d81b0210ea327280e1fee86e706e103b517195a3
Andrew Kozin <andrew.kozin@gmail.com>
2022-12-15 23:05:11 +0000

    Day 18-1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::{fs, io, num::ParseIntError, str::FromStr};
+use std::collections::HashMap;
+use bitmaps::Bitmap;
 
 #[derive(Debug)]
-enum Error {
+pub enum Error {
     ReadInput(io::Error),
-    ParseCalories(ParseIntError),
+    Parse(ParseIntError),
+    InvalidCoords,
 }
 impl From<io::Error> for Error {
     fn from(error: io::Error) -> Self {
@@ -12,16 +15,79 @@ impl From<io::Error> for Error {
 }
 impl From<ParseIntError> for Error {
     fn from(error: ParseIntError) -> Self {
-        Self::ParseCalories(error)
+        Self::Parse(error)
     }
 }
 
-fn main() {}
+#[derive(Debug, Default, Clone)]
+struct Crystal(HashMap<(usize, usize, usize), Bitmap<6>>);
+impl FromStr for Crystal {
+    type Err = Error;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let mut crystal = Self::default();
+        for line in input.lines() {
+            let mut items = line.split(',');
+            let x = items.next().ok_or(Error::InvalidCoords)?.parse::<usize>()?;
+            let y = items.next().ok_or(Error::InvalidCoords)?.parse::<usize>()?;
+            let z = items.next().ok_or(Error::InvalidCoords)?.parse::<usize>()?;
+            let mut cube = Bitmap::new();
+            for i in 0..6 {
+                cube.set(i, true);
+            }
+            crystal.0.insert((x, y, z), cube);
+        }
+        Ok(crystal)
+    }
+}
+impl Crystal {
+    pub fn load_from(path: &str) -> Result<Self, Error> {
+        Self::from_str(&fs::read_to_string(path)?)
+    }
+    
+    pub fn calculate(&mut self) {
+        let crystal = self.0.clone();
+        for (&(x, y, z), cube) in self.0.iter_mut() {
+            if x > 0 && crystal.contains_key(&(x - 1, y, z)) {
+                cube.set(0, false);
+            }
+            if crystal.contains_key(&(x + 1, y, z)) {
+                cube.set(1, false);
+            }
+            if y > 0 && crystal.contains_key(&(x, y - 1, z)) {
+                cube.set(2, false);
+            }
+            if crystal.contains_key(&(x, y + 1, z)) {
+                cube.set(3, false);
+            }
+            if z > 0 && crystal.contains_key(&(x, y, z - 1)) {
+                cube.set(4, false);
+            }
+            if crystal.contains_key(&(x, y, z + 1)) {
+                cube.set(5, false);
+            }
+        }
+    }
+    
+    pub fn sides(&self) -> usize {
+        self.0.iter().map(|(_, cube)| cube.len()).sum()
+    }
+}
+
+fn main() {
+    let mut crystal = Crystal::load_from("data/input.txt").unwrap();
+    crystal.calculate();
+    println!("The crystal has {} sides", crystal.sides());
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn result() {}
+    fn result() {
+        let mut crystal = Crystal::load_from("data/test.txt").unwrap();
+        crystal.calculate();
+        assert_eq!(crystal.sides(), 64);
+    }
 }
